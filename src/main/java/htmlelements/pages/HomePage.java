652de90dcc449ca176c8bf2bdd40f7c482196873
apaluch <apaluch@griddynamics.com>
2022-09-22 00:11:59 +0200

    changes for task 6

--- src/main/java/htmlelements/pages/HomePage.java
+++ src/main/java/htmlelements/pages/HomePage.java
@@ -4,15 +4,16 @@ import htmlelements.ExtendedMyWebElement;
 import htmlelements.ExtendedWebPage;
 import io.qameta.htmlelements.annotation.Description;
 import io.qameta.htmlelements.annotation.FindBy;
+import io.qameta.htmlelements.element.ExtendedList;
 
 public interface HomePage extends ExtendedWebPage {
     @Description("Privacy settings modal")
-    @FindBy("/html/body/div[4]/div")
-    ExtendedMyWebElement privacySettingsModal();
+    @FindBy("//*[@id=\"ember61\"]/div/div/button[1]")
+    ExtendedMyWebElement acceptPrivacyButton();
 
-    @Description("Header button")
-    @FindBy("/html/body/div[2]/div[4]/a")
-    ExtendedMyWebElement headerButton();
+    @Description("Header search button")
+    @FindBy("//*[@id=\"main-header\"]/div/div[1]/ul/li[2]/a")
+    ExtendedMyWebElement searchButton();
 
     @Description("Cart button")
     @FindBy("//*[@id=\"ember4\"]")
