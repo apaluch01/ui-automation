04586054d437f312d31955a4a708c174002e35f0
apaluch <apaluch@griddynamics.com>
2022-09-22 09:02:56 +0200

    added assertion

--- src/main/java/steps/SearchResultsPageSteps.java
+++ src/main/java/steps/SearchResultsPageSteps.java
@@ -3,16 +3,17 @@ package steps;
 import htmlelements.MyPageFactory;
 import htmlelements.MyPageFactoryProvider;
 import htmlelements.pages.SearchResultsPage;
+import org.junit.Assert;
 
 public class SearchResultsPageSteps {
     private static MyPageFactory pageFactory = MyPageFactoryProvider.getInstance();
 
-    private SearchResultsPage searchResultsPageSidebar() {
+    private SearchResultsPage searchResultsPage() {
         return pageFactory.on(SearchResultsPage.class);
     }
 
     public SearchResultsPageSteps verifyNumberOfProductIsDisplayed(int numProducts) {
-        // TODO: assertion
+        Assert.assertEquals(searchResultsPage().resultsList().size(), numProducts);
         return this;
     }
 }
