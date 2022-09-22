0fe5b6edee5cf17934d82f9260348888dca26c17
apaluch <apaluch@griddynamics.com>
2022-09-22 08:58:04 +0200

    second version of sixth task

---
+++ src/main/java/steps/OpenPageSteps.java
@@ -0,0 +1,20 @@
+package steps;
+
+import htmlelements.MyPageFactory;
+import htmlelements.MyPageFactoryProvider;
+import htmlelements.pages.HomePage;
+import org.aeonbits.owner.ConfigFactory;
+
+public class OpenPageSteps {
+    private static MyPageFactory pageFactory = MyPageFactoryProvider.getInstance();
+    //Configuration cfg = ConfigFactory.create(Configuration.class);
+
+    private HomePage homePage() {
+        return pageFactory.on(HomePage.class);
+    }
+
+    public HomePageSteps openHomepage() {
+        //homePage().open(cfg.homepageUrl());
+        return new HomePageSteps();
+    }
+}
