b063c9cdbd6e1b33935124a4b02250a93e951b6d
apaluch <apaluch@griddynamics.com>
2022-09-25 12:17:28 +0200

    account creation xpaths added

---
+++ src/main/java/htmlelements/pages/CreateAccountPage.java
@@ -0,0 +1,48 @@
+package htmlelements.pages;
+
+import htmlelements.ExtendedMyWebElement;
+import htmlelements.ExtendedWebPage;
+import io.qameta.htmlelements.annotation.Description;
+import io.qameta.htmlelements.annotation.FindBy;
+
+public interface CreateAccountPage extends ExtendedWebPage {
+    @Description("Email input field")
+    @FindBy("//input[@name='login']")
+    ExtendedMyWebElement emailInput();
+
+    @Description("First name input field")
+    @FindBy("//input[@name='firstname']")
+    ExtendedMyWebElement firstNameInput();
+
+    @Description("Last name input field")
+    @FindBy("//input[@name='lastname']")
+    ExtendedMyWebElement lastNameInput();
+
+    @Description("Password input field")
+    @FindBy("//input[@name='password']")
+    ExtendedMyWebElement passwordInput();
+
+    @Description("Password confirmation input field")
+    @FindBy("//input[@name='confirm_password']")
+    ExtendedMyWebElement confirmPassword();
+
+    @Description("Zip code input field")
+    @FindBy("//input[@name='postalCode']")
+    ExtendedMyWebElement zipCodeInput();
+
+    @Description("Birthday month select field")
+    @FindBy("//select[@name='month']")
+    ExtendedMyWebElement monthSelect();
+
+    @Description("Birthday date select field")
+    @FindBy("//select[@name='day']")
+    ExtendedMyWebElement daySelect();
+
+    @Description("Accept terms checkbox")
+    @FindBy("//input[@name='acceptTerms']")
+    ExtendedMyWebElement acceptTerms();
+
+    @Description("Final create account button")
+    @FindBy("//button[@class='btn btn-primary qa-btn-register  btn-block']")
+    ExtendedMyWebElement createAccountConfirm();
+}
