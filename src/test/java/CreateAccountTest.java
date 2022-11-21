bbe97b8cf7be47b0388bdc5e1c6f5e2c0c5c64f7
apaluch <apaluch@griddynamics.com>
2022-11-22 00:50:48 +0100

    task 15 finished

--- src/test/java/CreateAccountTest.java
+++ src/test/java/CreateAccountTest.java
@@ -6,8 +6,12 @@ public class CreateAccountTest extends BaseTest{
     @Test
     public void validAccountShouldCreate() {
         new OpenPageSteps(driver.get()).openHomepage().acceptPrivacyModal().
-                clickAccountButton().openAccountCreationPage().inputFirstName((String) constants.data.get(NAME)).
-                inputLastName("XYZ").confirmPassword((String) constants.data.get(PASSWORD)).inputZipCode("90001").
-                chooseMonth("January").chooseDay("17");
+                clickAccountButton().openAccountCreationPage().
+                inputFirstName(accountData.getFirstName()).
+                inputLastName(accountData.getLastName()).
+                confirmPassword((String) constants.data.get(PASSWORD)).
+                inputZipCode(accountData.getZipCode()).
+                chooseMonth(accountData.getBirthMonth()).
+                chooseDay(accountData.getBirthDay());
     }
 }
