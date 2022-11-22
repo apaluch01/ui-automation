7e159ba20d0dc0ce1ecc55cd86187070f7fed378
apaluch <apaluch@griddynamics.com>
2022-11-22 22:48:29 +0100

    added logger

--- src/main/java/configuration/WebDriverFactory.java
+++ src/main/java/configuration/WebDriverFactory.java
@@ -6,15 +6,17 @@ import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 public class WebDriverFactory {
     private WebDriver driver;
     Configuration cfg = ConfigFactory.create(Configuration.class);
+    static final Logger LOGGER = Logger.getLogger(String.valueOf(WebDriverFactory.class));
+
     private WebDriver createChromeDriver(Configuration cfg) {
         ChromeDriverManager.chromedriver().browserVersion(cfg.seleniumVersion()).setup();
-        this.driver = ChromeDriverManager.chromedriver().create();
+        driver = ChromeDriverManager.chromedriver().create();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         return this.driver;
@@ -22,7 +24,7 @@ public class WebDriverFactory {
 
     private WebDriver createFirefoxDriver() {
         FirefoxDriverManager.firefoxdriver().setup();
-        this.driver = FirefoxDriverManager.firefoxdriver().create();
+        driver = FirefoxDriverManager.firefoxdriver().create();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         return this.driver;
@@ -32,19 +34,19 @@ public class WebDriverFactory {
         Browser browser = Browser.valueOf(cfg.browserName().toUpperCase());
 
         if (browser == Browser.FIREFOX) {
-            this.driver = createFirefoxDriver();
+            driver = createFirefoxDriver();
         } else {
-            this.driver = createChromeDriver(cfg);
+            driver = createChromeDriver(cfg);
         }
 
-        this.driver.get("http://" + cfg.homepageUrl());
-        this.driver.manage().window().setSize(new Dimension(cfg.browserWidth(), cfg.browserHeight()));
-        System.out.println("initialized"); //TODO: Change with log4j logger
+        driver.get("http://" + cfg.homepageUrl());
+        driver.manage().window().setSize(new Dimension(cfg.browserWidth(), cfg.browserHeight()));
+        LOGGER.info("initialized");
     }
 
     public void end(){
-        this.driver.quit();
-        System.out.println("ended"); //TODO: Change with log4j logger
+        driver.quit();
+        LOGGER.info("ended");
     }
 
     public WebDriver get(){
